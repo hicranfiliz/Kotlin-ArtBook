6dc309c5a86e839f0ee12eaa2b1b1389fcbaaa91
Hicran Filiz <h.filiz781@gmail.com>
2023-03-03 18:31:13 +0300

    feat: create menu

--- KotlinArtBook/app/src/main/java/com/example/kotlinartbook/MainActivity.kt
+++ KotlinArtBook/app/src/main/java/com/example/kotlinartbook/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.kotlinartbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.kotlinartbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +17,31 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+    }
+
+    // art menu'yu buraya baglamam gerekiyor. Bunun icin 2 fonksiyonu override etmem gerekli.
+    // onCreate'te baglama islemini yapacagiz.
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        //inflater : ne zaman xml ile kodu birbirine baglamaya calisacaksak inflate kullaniyoruz.
+        // Ama yukarida nasil layoutInflater cagiriyorsak burada da menuInflater cagiracagiz.
+
+        val menuInflater = menuInflater
+        // Bizden 2 parametre ister: 1. olusturdugumuz menu(R.menu diyerek ulasabiliriz.)
+        // 2.hangi menu ile baglayacagim
+        menuInflater.inflate(R.menu.art_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    // onoptions'ta menuye tiklairsa ne olacak onu yapacagiz.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_art_item) {
+            val intent = Intent(this@MainActivity, ArtActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
